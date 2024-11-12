6d1fa6e454f59af5e4022f61c4f818c5ff21abbf
Bob5768 <601775@buchananschools.com>
2024-11-12 12:10:06 -0500

    Task one done

--- ex02/script.js
+++ ex02/script.js
@@ -1,14 +1,21 @@
 // Write your Task 1 code inside this function!
 function task1() {
-  console.log("Running task 1");
+  document.querySelector("code");
+  //target DIV//
+  let target = document.querySelector("#text-content")
+  console.log(target)
+  //set innertext to something new//
+target.innerText = "something new"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  
 }
 
 // Write your task 3 code inside this function!
 function task3() {
   console.log("Running task 3");
 }
+
