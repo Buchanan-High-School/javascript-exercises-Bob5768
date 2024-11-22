9dbde5b44d37edcfdef15e6207098916022419bc
Bob5768 <601775@buchananschools.com>
2024-11-22 10:45:01 -0500

    Task one done (this was difficult)
    issue: ORDER MATTERS, me no likey

--- ex04/script.js
+++ ex04/script.js
@@ -1,15 +1,17 @@
 function task1() {
   let myArray = ["one",
     "two",
-    "three",
-    "four"];
+    "three"];
   // This will run when you click the button.
+  let target = document.querySelector('#task-1-target')
+
   myArray.forEach((item) => {
     let el = document.createElement("p");
     el.innerText = item;
     target.appendChild(el);
   });
 }
+  let newEl = document.createElement("li")
 
 function task2A() {
   // Write your code in here
